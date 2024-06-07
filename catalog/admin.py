a676e5400d8cbb4dd2236da214c8febd8bcc50da
bruno somda <brunosomda01@gmail.com>
2024-06-07 14:28:29 +0000

    admin gestion

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from .models import Genre,Author,Language,Book,BookInstance
+
 # Register your models here.
+#@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+
+    list_display=['id','title','author','display_genre']
+
+   
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display=['first_name','last_name','date_of_birth','date_of_death']
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display=['id','imprint','book','status']
+
+
+admin.site.register(Book,BookAdmin)
+admin.site.register(Genre)
+admin.site.register(Language)
--- catalog/models.py
+++ catalog/models.py
@@ -1,13 +1,29 @@
 import uuid
+from django.db.models import UniqueConstraint
+from django.db.models.functions import Lower
 from django.db import models
 from django.urls import reverse
 # Create your models here.
+
 class Genre(models.Model):
     name = models.CharField(max_length=200,help_text='Enter a book genre (e.g. Science Fiction)')
     
     def __str__(self):
 
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("genre-detail",args=[str(self.id)])
+    
+    class Meta:
+        
+        constraints = [
+            UniqueConstraint(
+                Lower('name'),
+                name='genre_name_case_insensitive_unique',
+                violation_error_message = "Genre already exists (case insensitive match)"
+            ),
+        ]
     
 
 class Book(models.Model):
@@ -16,12 +32,18 @@ class Book(models.Model):
     summary = models.TextField(max_length=1000,help_text='Enter a brief description of the book')
     isbn = models.CharField('ISBN',max_length=13,help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre =models.ManyToManyField(Genre,help_text='Select a genre for this book')
+    language =models.ForeignKey("Language",on_delete=models.SET_NULL,null=True)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('book-detail',args=[str(self.id)])
+    
+    def display_genre(self):
+      
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    display_genre.short_description ='Genre'
 
 
 class BookInstance (models.Model):
@@ -61,3 +83,22 @@ class Author(models.Model):
 
     def __str__(self):
         return f'{self.last_name}, {self.first_name}'
+    
+class Language(models.Model):
+
+    name=models.CharField(max_length=200,unique=True,help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+    
+    def get_absolute_url(self):
+        return reverse('language-detail',args=[str(self.id)])
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                Lower('name'),
+                name='language_name_case_insensitive_unique',
+                violation_error_message = "Language already exists (case insensitive match)"
+            ),
+        ]
