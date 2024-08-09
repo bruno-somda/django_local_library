63b358d758013494b31e799e2f3f5c5e2e4c57ee
unknown <brunosomd01@gmail.com>
2024-08-09 14:55:03 +0000

    author et detail

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('books/',views.BookListView.as_view(),name='books'),
-    path('book/<int:pk>',views.BookDetailView.as_view(),name='book'),
+    path('book/<int:pk>',views.BookDetailView.as_view(),name='book-detail'),
     path('authors/',views.AuthorListView.as_view(),name='authors'),
-   # path('author/',views.AuthorDetailView.as_view(),name='author')
+    path('author/<int:pk>',views.AuthorDetailView.as_view(),name='author-detail')
     ]
--- catalog/views.py
+++ catalog/views.py
@@ -27,6 +27,12 @@ def index(request):
 class BookListView(generic.ListView):
     model = Book
 
+class BookDetailView(generic.DetailView):
+    model = Book
+
 
 class AuthorListView(generic.ListView):
+    model = Author
+
+class AuthorDetailView(generic.DetailView):
     model = Author
